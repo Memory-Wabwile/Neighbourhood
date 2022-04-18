5af9f9f4432c00dc7cc5ba113626b6c29923327e
Memory-Wabwile <memorywabwile@gmail.com>
2022-04-18 13:19:29 +0300

    create the landing page and render it

--- neighbours/views.py
+++ neighbours/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def landing(request):
+    message = "landing page"
+
+    return render (request ,'landing.html' , {'message':message})
+
 def home(request):
     message = "Home page"
 
-    return render (request ,'home.html' , {'message':message})
+    return render (request,'home.html',{'message':message})
