06fe6e4a7cc38c92f57d11de2bd0602ff5d0791e
Memory-Wabwile <memorywabwile@gmail.com>
2022-04-18 20:30:42 +0300

    add database settings and register the models

--- neighbours/models.py
+++ neighbours/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 # Create your models here.
 class Neighbourhood(models.Model):
@@ -10,6 +11,8 @@ class Neighbourhood(models.Model):
     description = models.TextField()
     image = CloudinaryField('images')
     admin=models.ForeignKey(User,on_delete=models.CASCADE, related_name='admin') 
+    health = models.IntegerField(null=True, blank=True)
+    police = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -22,7 +25,7 @@ class Profile(models.Model):
     user_id = models.IntegerField()
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, blank=True, null=True)
     email = models.EmailField()
-    profile_pic = CloudinaryField('profile')
+    profile_pic = CloudinaryField('images')
     bio = models.TextField(max_length=300, blank=True)
 
     def __str__(self):
@@ -33,6 +36,22 @@ class Business(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey(User,on_delete=models.CASCADE,default = '')
     email = models.CharField(max_length=100, default = '')
-    neighbourhood = models.ForeignKey("Neighbourhood",on_delete=models.CASCADE, default='', null=True, blank=True)
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE, default='', null=True, blank=True)
     description = models.TextField( default = '')
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    post_image = CloudinaryField('images')
+    post_description =models.TextField()
+    user= models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+    neighborhood=models.ForeignKey(Neighbourhood,on_delete= models.CASCADE,null=True)
+    date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+
+
     
