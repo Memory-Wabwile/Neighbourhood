985e6920c516c70aa0919c238715ecf5eaacdec4
Memory-Wabwile <memorywabwile@gmail.com>
2022-04-19 05:42:35 +0300

    create the business form and the post form

--- neighbours/forms.py
+++ neighbours/forms.py
@@ -2,3 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile,Neighbourhood,Business,Post
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user',)
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user',)
